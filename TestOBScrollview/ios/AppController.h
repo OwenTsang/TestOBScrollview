ba48716d657a0a03ed6389c4f63b543c16da1601
OwenTsang <owentsang@OwenTsangs-MacBook-Air.local>
2013-06-09 19:12:54 +0800

    Initial Commit

---
+++ TestOBScrollview/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TestOBScrollviewAppController.h
+//  TestOBScrollview
+//
+//  Created by OwenTsang on 6/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ TestOBScrollview/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestOBScrollviewAppController.h
+//  TestOBScrollview
+//
+//  Created by OwenTsang on 6/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
